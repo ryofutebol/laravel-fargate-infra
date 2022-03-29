ed9479f8ba9c60b40c99aceb3f96607b7e853ad5
ryofutebol <ryo.futebol@gmail.com>
2022-03-29 23:17:50 +0900

    RDSの構築

---
+++ envs/prod/routing/foobar_internal/data.tf
@@ -0,0 +1,21 @@
+data "terraform_remote_state" "network_main" {
+  backend = "s3"
+
+  config = {
+    bucket  = "larave-fargate-test-tfstate"
+    key     = "${local.system_name}/${local.env_name}/network/main_v1.0.0.tfstate"
+    region  = "ap-northeast-1"
+    profile = "terraform"
+  }
+}
+
+data "terraform_remote_state" "db_foobar" {
+  backend = "s3"
+
+  config = {
+    bucket  = "larave-fargate-test-tfstate"
+    key     = "${local.system_name}/${local.env_name}/db/foobar_v1.0.0.tfstate"
+    region  = "ap-northeast-1"
+    profile = "terraform"
+  }
+}
